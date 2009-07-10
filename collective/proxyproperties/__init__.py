ee0a8ed2aa6f2b4683bd59a2146f043f387f25d8
Clayton Parker <me@claytron.com>
2009-07-10 16:15:56 +0000

    circumvent KSS site root with a findSiteRoot method

--- collective/proxyproperties/__init__.py
+++ collective/proxyproperties/__init__.py
@@ -3,8 +3,16 @@ from persistent import Persistent
 
 from zope.interface import implements
 from zope.app.component.hooks import getSite
+from zope.component import getMultiAdapter
 
 from Products.CMFCore.interfaces import IPropertiesTool
+
+# KSS 
+try:
+    from kss.core.interfaces import IKSSView
+    HAS_KSS = True
+except ImportError:
+    HAS_KSS = False
 
 from collective.proxyproperties.interfaces import IProxyPropertyAble
 
@@ -13,6 +21,22 @@ proxypropertiesMessageFactory = zope.i18nmessageid.MessageFactory("collective.pr
 
 _marker = object()
 PROXY_PROPERTIES = 'collective.proxyproperties.propertyoverrides'
+
+def findSiteRoot():
+    """Method to get the site root from the KSS
+    site root.  MADNESS.
+    """
+    site_root = getSite()
+    # If the KSS is in use, it has its own site root :(
+    if HAS_KSS and IKSSView.providedBy(site_root):
+        context = site_root.context
+        request = site_root.request
+        # get the portal via the plone util view
+        portal_state = getMultiAdapter((context, request),
+                                       name=u"plone_portal_state")
+        site_root = portal_state.portal()
+    return site_root
+
 
 class ProxyProperties(Persistent):
     """A proxy object to gather properties
@@ -27,7 +51,7 @@ class ProxyProperties(Persistent):
         #     the portal_properties tool.  I would like to
         #     be able to recurse up to get to the real prop
         #     sheet eventually.
-        portal = getSite()
+        portal = findSiteRoot()
         prop_sheets = portal.portal_properties.objectIds()
         if name in prop_sheets:
             parent_props = portal.portal_properties
@@ -35,12 +59,12 @@ class ProxyProperties(Persistent):
         return super(ProxyProperties, self).__getattribute__(name)
     
     def getProperty(self, name, default=None):
-        portal = getSite()
+        portal = findSiteRoot()
         prop_sheet = portal.portal_properties
         return prop_sheet.getProperty(name, default)
     
     def hasProperty(self, name):
-        portal = getSite()
+        portal = findSiteRoot()
         prop_sheet = portal.portal_properties
         return prop_sheet.hasProperty(name)
 
@@ -75,7 +99,7 @@ class FakePropertySheet(object):
         return self.unrestrictedTraverse(path, default)
     
     def _getProxyProperty(self, prop, default=None):
-        portal = getSite()
+        portal = findSiteRoot()
         # if for some reason we can't adapt, pass off to the
         # default property sheet
         try:
